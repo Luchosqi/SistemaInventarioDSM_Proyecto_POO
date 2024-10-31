94a843c1bb7c99a9533bc7ca9d5e026cca2addb3
Luis Jaramillo Olate. <l.jaramillo02@ufromail.cl>
2024-10-31 19:51:50 -0300

    a

--- testunitarios/src/main/java/org/example/Main.java
+++ testunitarios/src/main/java/org/example/Main.java
@@ -1,4 +1,4 @@
-package test.src.main.java.org.example;
+package org.example;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -7,5 +7,12 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
         }
+        // asfsaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
     }
+}
